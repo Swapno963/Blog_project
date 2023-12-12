62585935ec20e1b9075670cffe7bc8c27124d3b7
swapno963 <swapno963@gmail.com>
2023-12-13 03:15:16 +0600

    categori and post can be save to db

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from . import forms
 # Create your views here.
+def add_post(request):
+    if request.method == 'POST':
+        post_form = forms.PostForm(request.POST)
+        if post_form.is_valid():
+            redirect("add_post")
+            post_form.save()
+    else:
+        post_form = forms.PostForm()
+    return render(request,'add_post.html',{"form":post_form})
